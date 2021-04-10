068a3256a8dbde6d2f1be6747dcf0ec14657cec8
inda3 <inda3@live.jp>
2021-04-10 06:19:04 +0000

    ここまで完了：React.js + LaravelでSPA開発講座#08

--- TodoApp/resources/views/index.blade.php
+++ TodoApp/resources/views/index.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     </head>
     <body>
         <div id="app"></div>
     </body>
-    <script src="/js/index.js"></script>
+    <script src="{{mix('/js/index.js')}}"></script>
 </html>
--- TodoApp/webpack.mix.js
+++ TodoApp/webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.ts('resources/ts/index.tsx', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
